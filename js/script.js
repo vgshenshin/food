225839ae6c6042790b4a8491b3908399e423d018
vgshenshin <vg.shenshin@gmail.com>
2022-09-03 11:13:11 +0300

    added Timer in JS

--- js/script.js
+++ js/script.js
@@ -2,43 +2,108 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    // Tabs
+	// Tabs
 
-    const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
+	const tabs = document.querySelectorAll('.tabheader__item'),
+		  tabsContent = document.querySelectorAll('.tabcontent'),
+		  tabsParent = document.querySelector('.tabheader__items');
 
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
-        });
+	function hideTabContent() {
+		tabsContent.forEach(item => {
+			item.classList.add('hide');
+			item.classList.remove('show', 'fade');
+		});
 
-        tabs.forEach(tab => {
-            tab.classList.remove('tabheader__item_active');
-        });
-    }
+		tabs.forEach(tab => {
+			tab.classList.remove('tabheader__item_active');
+		});
+	}
 
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
-    }
+	function showTabContent(i = 0) {
+		tabsContent[i].classList.add('show', 'fade');
+		tabsContent[i].classList.remove('hide');
+		tabs[i].classList.add('tabheader__item_active');
+	}
 
-    hideTabContent();
-    showTabContent();
+	hideTabContent();
+	showTabContent();
 
-    tabsParent.addEventListener('click', (e) => {
-        const target = e.target;
+	tabsParent.addEventListener('click', (e) => {
+		const target = e.target;
 
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
-            });
-        }
-    });
+		if (target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((item, i) => {
+				if (target == item) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
 
+	
+	// Timer
+
+	const deadline = '2022-12-31:00:00:00';
+
+	function getTimeRemaining(endtime) {
+		let days, hours, minutes, seconds;
+		const t = Date.parse(endtime) - Date.parse(new Date());
+		
+		if (t <= 0) {
+			days = 0;
+			hours = 0;
+			minutes = 0;
+			seconds = 0;
+		} else {
+			days = Math.floor(t / (1000 * 60 * 60 * 24)),
+			hours = Math.floor(t / (1000 * 60 * 60) % 24),
+			minutes = Math.floor((t / (1000 * 60)) % 60),
+			seconds = Math.floor((t / 1000) % 60);
+		}
+		
+		return {
+			total: t,
+			days: days,
+			hours: hours,
+			minutes: minutes,
+			seconds: seconds
+		};
+	}
+
+	function getZero(time) {
+		if (time >= 0 && time < 10) {
+			return `0${time}`;
+		} else {
+			return time;
+		}
+	}
+
+	function setClock(selector, endtime) {
+		const timer = document.querySelector(selector),
+			  days = timer.querySelector('#days'),
+			  hours = timer.querySelector('#hours'),
+			  minutes = timer.querySelector('#minutes'),
+			  seconds = timer.querySelector('#seconds'),
+			  interval = setInterval(refreshClock, 1000);
+
+		refreshClock();
+
+		function refreshClock() {
+			const t = getTimeRemaining(endtime);
+
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			seconds.innerHTML = getZero(t.seconds);
+
+			if (t.total <= 0) {
+				clearInterval(interval);
+			}
+		}
+	}
+
+	setClock('.timer', deadline);
+
+	
 });
