f12b8fceaaf7fd724a753aea7c01d2469fc5e3cb
vgshenshin <vg.shenshin@gmail.com>
2022-09-20 10:58:00 +0300

    added hard slider - carousel in JS

--- js/script.js
+++ js/script.js
@@ -399,7 +399,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	// 4 - навешивание обраб событ на стрелки и если вправо то выполн ф-ция и показ след слайд и изменяет индекс +1 и наоборот
 	// 5 - определяем кол-во слайдов и помещаем в графу общего кол-ва слайдов на стр
 	// 6 - добавить ноль при нумерации
-
+/*
 	const slides = document.querySelectorAll('.offer__slide'),    //  получаем псевдомассив с контентом слайдов
 		  prev = document.querySelector('.offer__slider-prev'),   //  стрелочка назад
 		  next = document.querySelector('.offer__slider-next'),   //  стрелочка вперед
@@ -447,5 +447,81 @@ window.addEventListener('DOMContentLoaded', () => {
 	next.addEventListener('click', () => {
 		plusSlides(1);
 	});
+*/
 
+    // hard slider - carousel (lesson 93)
+    // в html добавить div обертку для слайдов
+
+	const slides = document.querySelectorAll('.offer__slide'),                               //  получаем псевдомассив с контентом слайдов
+          prev = document.querySelector('.offer__slider-prev'),                              //  стрелочка назад
+          next = document.querySelector('.offer__slider-next'),                              //  стрелочка вперед
+          total = document.querySelector('#total'),                                          //  цифра общего кол-ва слайдов
+          current = document.querySelector('#current'),                                      //  цифра текущего слайда
+          slidesWrapper = document.querySelector('.offer__slider-wrapper'),                  //  главная обертка слайдов - выступает в роли рамки для видимой части слайдов
+          slidesField = document.querySelector('.offer__slider-inner'),                      //  доп.обертка слайдов в к-ой будут все слайды по горизонтали
+          slideWidth = +window.getComputedStyle(slidesWrapper).width.replace(/[^0-9]/g,"");  //  ширина обертки (рамка чрз к-ую смотрим на слайды)
+          //  это computed св-ва CSS т.е. из браузера, вытаскиваем из них width (ширину) и т.к. величина в px, с помощью replace сохр в переменную только цифры, + изменяет строку в число
+    let slideIndex = 1,                                       //  индекс текущего слайда
+        offset = 0;                                           //  отступ, на сколько % сдвинулась доп.обертка со слайдами
+
+    if (slides.length < 10) {                                 // условие для добавления 0, если общ кол-во слайдов меньше 10
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent = slideIndex;     
+    }
+
+    slidesField.style.width = 100 * slides.length + '%';      //  делаем ширину доп.обертки 400%, т.е. зависит от кол-ва слайдов
+    slidesField.style.display = 'flex';                       //  помещаем слайды в эту доп.обертку гориз-но др за др
+    slidesField.style.transition = '0.5s all';                //  для плавного пролистывания слайдов
+
+    slidesWrapper.style.overflow = 'hidden';                  //  ограничиваем видимость рамки, чтобы слайды видны были только внутри рамки
+
+    slides.forEach(slide => {                                 //  устанавливаем слайдам одинаковую ширину
+        slide.style.width = `${slideWidth}px`;                //  берем за основу ширину рамки чрз к-ую будут видны слайды
+    });
+
+
+    next.addEventListener('click', () => {
+    if (offset == slideWidth * (slides.length - 1)) {         //  если отступ равен ширине 1ого слайда*на кол-во слайдов-1, т.е. последний слайд в рамке
+        offset = 0;                                           //  то переставляем отступ в 0, т.е. в начало всех слайдов
+    } else {
+        offset += slideWidth;                                 //  если не последний слайд то прибавляем к отступу ширину слайда и слайд будет смещаться
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;  //  сдвигает доп.обертку со слайдами влево
+
+    if (slideIndex == slides.length) {                         //  условие для добавления единицы к индексу текущего слайда
+        slideIndex = 1;
+    } else {
+        slideIndex++;
+    }
+
+    if (slideIndex < 10) {                                     //  условие для добавления 0, если индекс текущего слайда меньше 10
+        current.textContent = `0${slideIndex}`;
+    } else {
+        current.textContent = slideIndex;
+    }
+    });
+
+    prev.addEventListener('click', () => {                      //  при нажатии на кнопку prev проверяем
+    if (offset == 0) {                                          //  если отступ равен 0, т.е. первый слайд в рамке
+        offset = slideWidth * (slides.length - 1);              //  то переставляем отступ на последний слайд
+    } else {
+        offset -= slideWidth;                                   //  если не первый слайд, то минусуем у отступа ширину слайда и слайд будет смещаться
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {                                      //  условие для уменьшения на единицу индекса текущего слайда
+        slideIndex = slides.length;
+    } else {
+        slideIndex--;
+    }
+
+    if (slideIndex < 10) {                                      //  условие для добавления 0, если индекс текущего слайда меньше 10
+        current.textContent = `0${slideIndex}`;
+    } else {
+        current.textContent = slideIndex;
+    }
+    });
 });
