5b0367d7df0ad49d7e9117f636c53d9683ec730a
vgshenshin <vg.shenshin@gmail.com>
2024-02-08 15:11:06 +0300

    clearing comments

--- js/modules/forms.js
+++ js/modules/forms.js
@@ -2,11 +2,6 @@ import {closeModal, openModal} from './modal';
 import {postData} from '../services/services';
 
 function forms(formSelector, modalTimerId) {
-	//  Forms - принимаем данные пользователя с формы обр связи
-		// создаем в корне проекта файл для back-end'a - server.php
-		// исп-ем для отправки объект XMLHttpRequest
-		// пробуем для отправки данных 2 разных формата объект FormData и JSON
-		// для отправки JSON в server.php необходимо добавить: $_POST = json_decode(file_get_contents("php://input"), true);
 
 		const forms = document.querySelectorAll(formSelector);  //  получаем все формы обр связи
 
@@ -36,72 +31,12 @@ function forms(formSelector, modalTimerId) {
 				`;
 				
 				form.insertAdjacentElement('afterend', statusMessage);  //  и выводим под ФОС информируя пользователя о статусе отправки
-
-				
-				// const request = new XMLHttpRequest();  // это API, который предоставляет клиенту функциональность для обмена данными между клиентом и сервером
-				// request.open('POST', 'server.php');  //  настраиваем - POST отправку на сервер, второй арг это путь к серверу
-	
-				// request.setRequestHeader('Content-type', 'multipart/form-data');  //  настройка заголовка для объекта FormData
-				// связка XMLHttpRequest и FormData не работает с указанием заголовка, поэтому заголовок не пишем !!!
-
-				// request.setRequestHeader('Content-type', 'application/json');  //  настройка заголовка для объекта JSON
 	
 				const formData = new FormData(form);  // Объект FormData позволяет создать набор пар ключ/значение и передать их. На input'ах должен быть атрибут name!!!
 
-				const json = JSON.stringify(Object.fromEntries(formData.entries()));  // трнсформируем formData в JSON
-
-				//  создаем объект и помещаем в него данные из formData чтобы потом трансформировать их в JSON
-				// const object = {};
-				// formData.forEach(function(value, key){
-				//     object[key] = value;
-				// });
-
-				// const json = JSON.stringify(object);  //  трансформирует обычный объект в JSON
-				// request.send(json);  //  отправляем к серверу Объект JSON через XMLHttpRequest()
-
-				// request.send(formData);  //  отправляем к серверу Объект FormData через XMLHttpRequest()
-
-				//-------------------------fetch API------------------------------------------------------------------
-				// постим данные с ФОС на сервер с помощью fetch API
-				// 1. В виде FormData
-				// fetch('server.php', {           //  в файле server.php не забудь удалить строку с json_decode
-				//     method: "POST",
-				//     body: formData
-				// })
-				// .then(data => data.text())      //  трансформирует formData в текст чтобы вывести в консоль
-				// .then(data => {
-				//     console.log(data);
-				//     showThanksModal(message.success);
-				// }).catch(() => {
-				//     showThanksModal(message.failure);
-				// }).finally(() => {
-				//     form.reset();
-				//     statusMessage.remove();
-				// });
-
-				
-				// 2. В виде JSON
-				// fetch('server.php', {           //  в файле server.php не забудь прописать строку с json_decode
-				//     method: "POST",
-				//     headers: {
-				//         'Content-type': 'application/json'
-				//     },
-				//     body: json                  //  json - это переменная в к-ой хранятся данный с ФОС в JSON формате(стр.278-283) 
-				// })
-				// .then(data => data.text())      //  трансформирует JSON в текст чтобы вывести в консоль
-				// .then(data => {
-				//     console.log(data);
-				//     showThanksModal(message.success);
-				// }).catch(() => {
-				//     showThanksModal(message.failure);
-				// }).finally(() => {
-				//     form.reset();
-				//     statusMessage.remove();
-				// });
-
 				// исп-ем ф-цию для оптимизации кода по работе с сервером
 				postData('https://script.google.com/macros/s/AKfycbyILGctWH90IsZ99nFYKPETJWnYEVaEWouSs2w2vDu6IDFQu5Ckt_mLQg1dhRHBO3CRjw/exec', formData)        // из ф-ции вернется промис
-					.then(data => {
+					.then(() => {
 						showThanksModal(message.success);
 					}).catch(() => {
 						showThanksModal(message.failure);
@@ -109,27 +44,10 @@ function forms(formSelector, modalTimerId) {
 						form.reset();
 						statusMessage.remove();
 					});
-
-
-				//--------------------------------------------------------------------------------------------
-
-				//  слушаем событие load от XMLHttpRequest()
-				// request.addEventListener('load', () => {              //  отслеживаем конечную загрузку запроса
-				//     if (request.status === 200) {                     //  если запрос прошел статус ОК(200)
-				//         console.log(request.response);                //  выведем в консоль объект с данными из ФОС
-				//         showThanksModal(message.success);             //  вызываем ф-цию показа окна благодарности
-				//         form.reset();                                 //  очистка ФОС от введенных данных
-				//         statusMessage.remove();                       //  очищаем сообщ польз о загрузке (спиннер)
-				//     } else {                                          //  негативный сценарий если запрос НЕ прошел статус ОК(200)
-				//         showThanksModal(message.failure);             //  вызываем ф-цию показа окна с сообщ польз о проблеме в отправке
-				//     }
-				// });
 			});
 		}
-	
 
 		//  создаем окно благодарности (после отправки данных)
-
 		function showThanksModal(message) {
 			const prevModalDialog = document.querySelector('.modal__dialog');  //  получаем существующее мод окно в переменную
 	
--- js/modules/slider.js
+++ js/modules/slider.js
@@ -1,83 +1,21 @@
 function slider({container, slide, nextArrow, prevArrow, totalCounter, currentCounter, wrapper, field}) {
-	// simple slider (lesson 92)
 
-	// алгоритм работы по созданию слайдера
-	// 1 - получить эл-ты
-	// 2 - сделать индекс к-ый определяет текущий слайд, его будем исп-ть в числовом обозначении сверху и изменять
-	// 3 - написание ф-ции к-ая будем заниматься показом слайдов. Будет две ф-ции)
-	//     - показ какого то опред слайда
-	//     - скрытие других, к-ых не видим (принимает индекс, по этому индексу показывает, а оставшиеся скрывает), и 
-	//       проверяет условие - когда дошли до конца => переходим на 1 слайд и наоборот
-	// 4 - навешивание обраб событ на стрелки и если вправо то выполн ф-ция и показ след слайд и изменяет индекс +1 и наоборот
-	// 5 - определяем кол-во слайдов и помещаем в графу общего кол-ва слайдов на стр
-	// 6 - добавить ноль при нумерации
-/*
-	const slides = document.querySelectorAll('.offer__slide'),    //  получаем псевдомассив с контентом слайдов
-		  prev = document.querySelector('.offer__slider-prev'),   //  стрелочка назад
-		  next = document.querySelector('.offer__slider-next'),   //  стрелочка вперед
-		  total = document.querySelector('#total'),               //  цифра общего кол-ва слайдов
-		  current = document.querySelector('#current');           //  цифра текущего слайда
-	let slideIndex = 1;                                       //  индекс текущего слайда
-
-	showSlides(slideIndex);                                   // запускаем ф-цию показа и скрытия слайдов(арг - Индекс текущего слайда = 1)
-
-	if (slides.length < 10) {                                 // условие для добавления 0, если общ кол-во слайдов меньше 10
-		total.textContent = `0${slides.length}`;
-	} else {
-		total.textContent = slides.length;        
-	}
-
-	function showSlides(n) {                                 // ф-ция показа и скрытия слайдов(арг = Индекс текущего слайда)
-		if (n > slides.length) {                             // условие: когда долистали до конца всех слайдов
-			slideIndex = 1;                                  // присвоить Индексу текущего слайда 1, т.е. вернуть в начало
-		}
-															 
-		if (n < 1) {                                         //  условие: когда долистали до начала всех слайдов
-			slideIndex = slides.length;                      // присвоить Индексу текущего слайда число равное кол-ву слайдов в псевдомассиве, т.е. вернуть в конец
-		}
-
-		slides.forEach(item => item.classList.add('hide'));  //  скрываем все слайды
-
-		//  когда индекс текущего слайда равен 1, ни одно условие не сработает и цифра 1 подставится ниже в индекс
-		slides[slideIndex - 1].classList.remove('hide');
-
-		if (slideIndex < 10) {                           // условие для добавления 0, если индекс текущего слайда меньше 10
-			current.textContent = `0${slideIndex}`;
-		} else {
-			current.textContent = slideIndex;
-		}
-	}
-
-	function plusSlides(n) {
-		showSlides(slideIndex += n);
-	}
-
-	prev.addEventListener('click', () => {
-		plusSlides(-1);
-	});
-
-	next.addEventListener('click', () => {
-		plusSlides(1);
-	});
-*/
-
-	// hard slider - carousel (lesson 93)
-	// в html добавить div обертку для слайдов
-
-	const slider = document.querySelector(container),   //  весь слайдер
-		  slides = document.querySelectorAll(slide),                               //  получаем псевдомассив с контентом слайдов
-		  prev = document.querySelector(prevArrow),                              //  стрелочка назад
-		  next = document.querySelector(nextArrow),                              //  стрелочка вперед
-		  total = document.querySelector(totalCounter),                                          //  цифра общего кол-ва слайдов
-		  current = document.querySelector(currentCounter),                                      //  цифра текущего слайда
-		  slidesWrapper = document.querySelector(wrapper),                  //  главная обертка слайдов - выступает в роли рамки для видимой части слайдов
-		  slidesField = document.querySelector(field),                      //  доп.обертка слайдов в к-ой будут все слайды по горизонтали
-		  slideWidth = +window.getComputedStyle(slidesWrapper).width.replace(/\D/g, "");	 //  ширина обертки (рамка чрз к-ую смотрим на слайды)
+	const slider = document.querySelector(container),   					//  весь слайдер
+		  slides = document.querySelectorAll(slide),						//  получаем псевдомассив с контентом слайдов
+		  prev = document.querySelector(prevArrow),							//  стрелочка назад
+		  next = document.querySelector(nextArrow),							//  стрелочка вперед
+		  total = document.querySelector(totalCounter),						//  цифра общего кол-ва слайдов
+		  current = document.querySelector(currentCounter),					//  цифра текущего слайда
+		  slidesWrapper = document.querySelector(wrapper),					//  главная обертка слайдов - выступает в роли рамки для видимой части слайдов
+		  slidesField = document.querySelector(field),						//  доп.обертка слайдов в к-ой будут все слайды по горизонтали
+		  slideWidth = +window.getComputedStyle(slidesWrapper).width.replace(/\D/g, "");	//  ширина обертки (рамка чрз к-ую смотрим на слайды)
 		  //  это computed св-ва CSS т.е. из браузера, вытаскиваем из них width (ширину) и т.к. величина в px, с помощью replace сохр в переменную только цифры, + изменяет строку в число
-	let slideIndex = 1,                                       //  индекс текущего слайда
-		offset = 0;                                           //  отступ, на сколько % сдвинулась доп.обертка со слайдами
+	
+	
+	let slideIndex = 1,							//  индекс текущего слайда
+		offset = 0;								//  отступ, на сколько % сдвинулась доп.обертка со слайдами
 
-	if (slides.length < 10) {                                 // условие для добавления 0, если общ кол-во слайдов меньше 10
+	if (slides.length < 10) {									// условие для добавления 0, если общ кол-во слайдов меньше 10
 		total.textContent = `0${slides.length}`;
 		current.textContent = `0${slideIndex}`;
 	} else {
@@ -85,14 +23,14 @@ function slider({container, slide, nextArrow, prevArrow, totalCounter, currentCo
 		current.textContent = slideIndex;     
 	}
 
-	slidesField.style.width = 100 * slides.length + '%';      //  делаем ширину доп.обертки 400%, т.е. зависит от кол-ва слайдов
-	slidesField.style.display = 'flex';                       //  помещаем слайды в эту доп.обертку гориз-но др за др
-	slidesField.style.transition = '0.5s all';                //  для плавного пролистывания слайдов
+	slidesField.style.width = 100 * slides.length + '%';		//  делаем ширину доп.обертки 400%, т.е. зависит от кол-ва слайдов
+	slidesField.style.display = 'flex';							//  помещаем слайды в эту доп.обертку гориз-но др за др
+	slidesField.style.transition = '0.5s all';					//  для плавного пролистывания слайдов
 
-	slidesWrapper.style.overflow = 'hidden';                  //  ограничиваем видимость рамки, чтобы слайды видны были только внутри рамки
+	slidesWrapper.style.overflow = 'hidden';					//  ограничиваем видимость рамки, чтобы слайды видны были только внутри рамки
 
-	slides.forEach(slide => {                                 //  устанавливаем слайдам одинаковую ширину
-		slide.style.width = `${slideWidth}px`;                //  берем за основу ширину рамки чрз к-ую будут видны слайды
+	slides.forEach(slide => {									//  устанавливаем слайдам одинаковую ширину
+		slide.style.width = `${slideWidth}px`;					//  берем за основу ширину рамки чрз к-ую будут видны слайды
 	});
 
 
